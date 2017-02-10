237c10fea606874cefe06527201bb6d25f0ddf59
jawahar <jawahar273@gmail.com>
2017-02-10 11:29:19 +0530

    Update mainsrl.py

--- mainsrl.py
+++ mainsrl.py
@@ -7,8 +7,9 @@ Created on Wed Jan 25 10:22:51 2017
 """
 
 from srlnltk import SennaSRLTagger 
-s = SennaSRLTagger('/media/jawahar/jon/ubuntu/senna')
+s = SennaSRLTagger('') # pls add dir if senna is not install
 #w = s.tag("Are you studying here?".split())
 w = s.tag("""A general interface to the SENNA pipeline that supports any of the operations specified in SUPPORTED OPERATIONS..""".split())#,file_mode='w')
-s.tag2file("""A general interface to the SENNA pipeline that supports any of the operations specified in SUPPORTED OPERATIONS..""".split())
+#.tag2file("""A general interface to the SENNA pipeline that supports any of the operations specified in SUPPORTED OPERATIONS..""".split())
+# create a file on current dir default
 print(w)
