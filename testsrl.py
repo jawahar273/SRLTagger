c934b40b9271cf68778034f7d1537754c194b6b0
jawahar <jawahar273@gmail.com>
2017-04-08 13:18:52 +0530

    Update testsrl.py

--- testsrl.py
+++ testsrl.py
@@ -7,9 +7,24 @@ Created on Wed Jan 25 10:22:51 2017
 """
 
 from srlnltk import SennaSRLTagger 
-s = SennaSRLTagger('') # pls add dir if senna is not install
+from nltk.tag import SennaTagger
+sents = """He killed the man with a knife and murdered him with a dagger.""".split()
+path = '/media/jawahar/jon/ubuntu/senna'
+from nltk.tag import SennaChunkTagger
+from nltk.tag import SennaNERTagger
+
+srltagger = SennaSRLTagger(path)
+nertagger = SennaNERTagger(path)
+chktagger = SennaChunkTagger(path)
+tagger = SennaTagger(path)
+
+
 #w = s.tag("Are you studying here?".split())
-w = s.tag("""A general interface to the SENNA pipeline that supports any of the operations specified in SUPPORTED OPERATIONS..""".split())#,file_mode='w')
-#.tag2file("""A general interface to the SENNA pipeline that supports any of the operations specified in SUPPORTED OPERATIONS..""".split())
-# create a file on current dir default
-print(w)
+#w = s.tag("""A general interface to the SENNA pipeline that supports any of the operations specified in SUPPORTED OPERATIONS..""".split()) 
+
+#print(tagger.tag(sents))
+#print(chktagger.tag(sents))
+#print(nertagger.tag(sents))
+#print(srltagger.tag(sents))
+
+#s.tag2file("""A general interface to the SENNA pipeline that supports any of the operations specified in SUPPORTED OPERATIONS..""".split())
